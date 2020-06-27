3fb0809e3a9d75ddd12061189769f73761700659
Gracious Saxena <43841726+GraciousSaxena@users.noreply.github.com>
2020-06-27 23:47:52 +0530

    Create (Medium) Jewel Game.cpp

---
+++ (Medium) Jewel Game.cpp
@@ -0,0 +1,40 @@
+// Jewel Game
+#include <bits/stdc++.h>
+#define endl "\n"
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define forn(i,n) for (int i = 0; i < int(n); ++i)
+#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define w(x) int x; cin >> x; while (x--)
+
+using namespace std;
+typedef long long ll;
+
+bool cmp(pair<int, int> a, pair<int, int> b){
+    return a.second<b.second;
+}
+
+
+int main(){
+    IOS;
+    w(t){
+        string s;   cin>>s;
+        int cnt=0;
+        forn(idx, s.size()-1){
+            int i = idx;
+            if(s[i] == s[i+1]){
+                s.erase(i, 1);
+                s.erase(i, 1);
+                if(idx>0)
+                    idx-=2;
+                cnt++;
+            }
+        }
+        cout<<cnt<<endl;
+    }
+    return 0;
+}
